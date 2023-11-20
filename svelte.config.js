f166eb27040d0aedb90d6bf67d13ff4a461aa87d
snehith03 <122145839+snehith03@users.noreply.github.com>
2023-11-21 00:19:31 +0530

    Add files via upload

---
+++ svelte.config.js
@@ -0,0 +1,16 @@
+import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-vercel';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	 kit: {
+    adapter: adapter({
+      maxDuration: 60,
+    }),
+  },
+}
+export default config;
